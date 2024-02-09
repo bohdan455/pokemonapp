77a08e4dbceacab90ca9121d9311042ab40988eb
Bohdan <bohdan_voroniak@epam.com>
2024-02-09 23:57:08 +0200

    add endpoint configuration

---
+++ src/enviroment/configuration.endpoints.ts
@@ -0,0 +1,5 @@
+export const configurationEndpoints = {
+    allPokemons: 'https://pokeapi.co/api/v2/pokemon',
+    pokemonById: 'https://pokeapi.co/api/v2/pokemon/{id}',
+    pokemonImage: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/{id}.png',
+}
